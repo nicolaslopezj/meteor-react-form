1ca17f391f25390a79fcc9ca16ad88c0d3d6527f
Nicolás López <nicolaslopezj@me.com>
2016-02-04 13:24:58 -0300

    Improve string, number and textarea components

--- packages/material-ui/components/string.jsx
+++ packages/material-ui/components/string.jsx
@@ -10,29 +10,15 @@ MRF.Components.String = React.createClass({
     onChange: React.PropTypes.func.isRequired,
   },
 
-  getInitialState() {
-    return {
-      value: this.props.value,
-    };
-  },
-
-  componentWillReceiveProps: function(nextProps) {
-    if (nextProps.value !== this.props.value) {
-      this.setState({ value: nextProps.value });
-    }
-  },
-
   render: function() {
     return (
       <TextField
         ref="input"
         fullWidth={true}
-        value={this.state.value}
+        value={this.props.value}
         floatingLabelText={this.props.label}
         errorText={this.props.errorMessage}
-        onChange={(event) => this.setState({ value: event.target.value })}
-        onBlur={(event) => this.props.onChange(event.target.value)}
-        onEnterKeyDown={() => this.props.onChange(this.state.value)} />
+        onChange={(event) => this.props.onChange(event.target.value)}/>
     );
   },
 });
