f305bc80e90c2be03b8a0ef9a4e09b6dae6da996
Nicolás López <nicolaslopezj@me.com>
2016-02-12 13:51:39 -0300

    Add checkbox attribute

---
+++ packages/material-ui/components/checkbox.jsx
@@ -0,0 +1,37 @@
+var {
+  Checkbox,
+  Styles,
+} = MUI;
+
+var {
+  Colors,
+} = Styles;
+
+class CheckboxComponent extends MRF.FieldType {
+  render() {
+    return (
+      <div style={{ paddingTop: 10, paddingBottom: 10 }}>
+        <Checkbox
+          checked={this.props.value}
+          onCheck={() => this.props.onChange(!this.props.value)}
+          errorText={this.props.errorMessage}
+          label={this.props.label}
+        />
+      <span style={{ color: Colors.red500 }}>{this.props.errorMessage}</span>
+      </div>
+    );
+  }
+}
+
+MRF.registerAttribute({
+  type: 'Checkbox',
+  component: CheckboxComponent,
+  schema: {
+    type: Boolean,
+  },
+});
+
+MRF.registerAttribute({
+  type: 'Boolean',
+  component: CheckboxComponent,
+});
