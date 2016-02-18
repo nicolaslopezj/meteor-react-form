d8c44ac906316259c11f1d7466e2282c6471073f
Nicolás López <nicolaslopezj@me.com>
2016-02-18 18:14:38 -0300

    changes versions

--- packages/mrf/package.js
+++ packages/mrf/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'nicolaslopezj:mrf',
-  version: '0.3.2',
+  version: '0.4-modules-beta.0',
   summary: 'Automatic forms creation with Simple Schema and React',
   git: 'https://github.com/nicolaslopezj/meteor-react-form',
   documentation: 'README.md',
