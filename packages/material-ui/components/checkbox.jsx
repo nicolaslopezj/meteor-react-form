d5b9b4d89bfd4a1b3740e2c9d4dccd7d45710066
Nicolás López <nicolaslopezj@me.com>
2016-03-17 15:44:01 -0300

    fix icon button

--- packages/material-ui/components/checkbox.jsx
+++ packages/material-ui/components/checkbox.jsx
@@ -3,6 +3,7 @@ import MRF from 'meteor/nicolaslopezj:mrf';
 
 var {
   Checkbox,
+  FontIcon,
   Styles,
 } = MUI;
 
@@ -11,18 +12,28 @@ var {
 } = Styles;
 
 class CheckboxComponent extends MRF.FieldType {
+
+  checkedIcon() {
+    return <FontIcon className="material-icons">check_box</FontIcon>
+  }
+
+  unCheckedIcon() {
+    return <FontIcon className="material-icons">check_box_outline_blank</FontIcon>
+  }
+
   render() {
     return (
       <div style={{ paddingTop: 10, paddingBottom: 10 }}>
         <Checkbox
-          checked={this.props.value}
-          onCheck={() => this.props.onChange(!this.props.value)}
-          errorText={this.props.errorMessage}
           label={this.props.label}
           disabled={this.props.disabled}
+          checked={this.props.value}
+          onCheck={() => this.props.onChange(!this.props.value)}
+          checkedIcon={this.checkedIcon()}
+          unCheckedIcon={this.unCheckedIcon()}
           {...this.passProps}
         />
-      <span style={{ color: Colors.red500 }}>{this.props.errorMessage}</span>
+        <span style={{ color: Colors.red500 }}>{this.props.errorMessage}</span>
       </div>
     );
   }
